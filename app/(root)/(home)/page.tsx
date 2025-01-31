9a5bdd037d0def38cad1dbda625417fb5038ef30
Rishabh <github@aperta.anonaddy.com>
2025-01-31 16:00:22 +0530

    initial commit

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,163 @@
+// "use client";
+import React from "react";
+import Image from "next/image";
+import { BiPhoneCall } from "react-icons/bi";
+import { FaWhatsapp } from "react-icons/fa";
+import { RiContactsLine } from "react-icons/ri";
+import { IoMdMail } from "react-icons/io";
+import { MdFacebook } from "react-icons/md";
+import { AiFillInstagram } from "react-icons/ai";
+import { HiMiniGlobeAsiaAustralia } from "react-icons/hi2";
+
+const page = () => {
+  return (
+    <>
+      <main className="relative mx-auto mt-8 flex max-w-[90%] flex-col items-center justify-center overflow-clip">
+        {/* TOP INFO W LOGO */}
+        <section className="flex flex-col justify-between text-center text-lg md:text-xl">
+          <Image
+            src="/images/celeste logo.png"
+            alt=""
+            width={500}
+            height={500}
+            className="mb-10 w-28 place-self-center md:mb-10 md:w-44"
+          />
+          <h1 className="text-3xl font-semibold md:text-4xl">
+            Céleste Consulting
+          </h1>
+          <h1 className="mb-10 text-lg tracking-wide opacity-75 md:text-xl">
+            Helping Dermatologists with <br /> Patient Acquisition
+          </h1>
+          {/* <p>Hey! 👋</p>
+          <p>
+            This is my digital business card. Here&apos;s everything to know
+            about me.
+          </p> */}
+          {/* <div className="my-4 border-2 border-b-black-100 md:my-8" /> */}
+        </section>
+
+        {/* HERO + 3 BUTTONS */}
+        <section>
+          {/* <Image
+            src="/images/doctor.jpg"
+            alt=""
+            width={500}
+            height={500}
+            className="mx-auto my-2 w-max place-self-center rounded-2xl"
+          /> */}
+          <div className="mx-auto">
+            <a
+              className="my-4 flex items-center justify-center md:mt-5"
+              href="tel:+916362840780"
+            >
+              <button className="w-[90vw] rounded-xl bg-indigo-300 p-3 text-base transition-all duration-300 hover:bg-indigo-500 hover:text-white active:bg-black-100 active:text-white md:w-[80vw] md:p-3 md:px-6 lg:w-[25vw]">
+                <span className="ml-4 flex items-center justify-start gap-x-4 md:ml-12 md:gap-x-6">
+                  <BiPhoneCall className="text-2xl md:text-4xl" />
+                  Call Now
+                </span>
+              </button>
+            </a>
+            <a
+              className="my-4 flex items-center justify-center md:mt-5"
+              href="https://api.whatsapp.com/send?phone=919557088564"
+            >
+              <button className="w-[90vw] rounded-xl bg-indigo-300 p-3 text-base transition-all duration-300 hover:bg-indigo-500 hover:text-white active:bg-black-100 active:text-white md:w-[80vw] md:p-3 md:px-6 lg:w-[25vw]">
+                <span className="ml-4 flex items-center justify-start gap-x-4 md:ml-12 md:gap-x-6">
+                  <FaWhatsapp className="text-2xl md:text-4xl" />
+                  Text on WhatsApp
+                </span>
+              </button>
+            </a>
+            <a
+              className="my-4 flex items-center justify-center md:mt-5"
+              href="/Rishabh Céleste Consulting.vcf"
+              download={"Rishabh Céleste Consulting"}
+            >
+              <button className="w-[90vw] rounded-xl bg-indigo-300 p-3 text-base transition-all duration-300 hover:bg-indigo-500 hover:text-white active:bg-black-100 active:text-white md:w-[80vw] md:p-3 md:px-6 lg:w-[25vw]">
+                <span className="ml-4 flex items-center justify-start gap-x-4 md:ml-12 md:gap-x-6">
+                  <RiContactsLine className="text-2xl md:text-4xl" />
+                  Save Contact Info
+                </span>
+              </button>
+            </a>
+          </div>
+          {/* <div className="my-4 border-2 border-b-black-100 md:my-8" /> */}
+        </section>
+
+        {/* VISIT */}
+        {/* <section className="flex flex-col justify-between text-center text-lg">
+          <h1 className="my-2 mb-4 text-3xl font-semibold md:text-5xl">
+            Visit Us
+          </h1>
+          <div className="mx-auto mb-2 aspect-square w-[90vw] md:w-[80vw] lg:w-[25vw]">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22582.143987612282!2d-122.1565248820646!3d37.42647615735572!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fbae5f33774b5%3A0x6dd7ef2d054587e3!2sTerun%20Pizza!5e0!3m2!1sen!2sin!4v1735704949104!5m2!1sen!2sin"
+              width="100%"
+              height="100%"
+              className="rounded-2xl border-0"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+          <h1 className="my-5 text-2xl md:text-3xl">Working Hours</h1>
+          <div className="mx-auto grid w-[85vw] grid-cols-2 justify-center md:w-[70vw] md:text-xl lg:w-[25vw]">
+            <div className="text-left md:leading-relaxed">
+              <p>Monday</p>
+              <p>Tuesday</p>
+              <p>Wednesday</p>
+              <p>Thursday</p>
+              <p>Friday</p>
+              <p>Saturday</p>
+              <p>Sunday</p>
+            </div>
+            <div className="text-right opacity-75 md:leading-relaxed">
+              <p>11 am to 5 pm</p>
+              <p>11 am to 5 pm</p>
+              <p>11 am to 5 pm</p>
+              <p>11 am to 5 pm</p>
+              <p>11 am to 5 pm</p>
+              <p>11 am to 1 pm</p>
+              <p>11 am to 1 pm</p>
+            </div>
+          </div>
+          <div className="my-4 border-2 border-b-black-100 md:my-8" />
+        </section> */}
+
+        {/* SOCIAL ICONS */}
+        <section className="mb-20 mt-5 flex flex-row items-center justify-center space-x-4 text-3xl md:mb-24 md:space-x-6 md:text-4xl">
+          <a href="mailto:hello@celesteconsulting.in" target="_blank">
+            <IoMdMail />
+          </a>
+          <a href="https://www.celesteconsulting.in/" target="_blank">
+            <HiMiniGlobeAsiaAustralia />
+          </a>
+          <a
+            href="https://www.facebook.com/people/Celeste-Consulting/61561819893146/"
+            target="_blank"
+          >
+            <MdFacebook />
+          </a>
+          <a href="https://www.instagram.com/consultceleste/" target="_blank">
+            <AiFillInstagram />
+          </a>
+        </section>
+      </main>
+
+      {/* <footer>
+        <div className="bg-black p-3 text-center text-sm font-light text-white md:p-6 md:text-lg lg:text-xl">
+          {" "}
+          Designed by{" "}
+          <a
+            href="https://www.celesteconsulting.in/"
+            className="!cursor-pointer hover:text-indigo-300"
+            target="_blank"
+          >
+            {" "}
+            Céleste Consulting ✨
+          </a>
+        </div>
+      </footer> */}
+    </>
+  );
+};
+
+export default page;
---
+++ tailwind.config.ts
@@ -0,0 +1,185 @@
+import type { Config } from "tailwindcss";
+
+const svgToDataUri = require("mini-svg-data-uri");
+
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        lexend: ["Lexend", "Helvetica", "Arial", "sans-serif"],
+        lexendDeca: ["LexendDeca", "Helvetica", "Arial", "sans-serif"],
+        inter: ["Inter", "Helvetica", "Arial", "sans-serif"],
+        opensans: ["OpenSans", "Helvetica", "Arial", "sans-serif"],
+        // poppinsRegular: ["PoppinsRegular", "Helvetica", "Arial", "sans-serif"],
+        // poppinsBlack: ["PoppinsBlack", "Helvetica", "Arial", "sans-serif"],
+        // poppinsBold: ["PoppinsBold", "Helvetica", "Arial", "sans-serif"],
+        // poppinsExtraBold: [
+        //   "PoppinsExtraBold",
+        //   "Helvetica",
+        //   "Arial",
+        //   "sans-serif",
+        // ],
+        // poppinsSemiBold: [
+        //   "PoppinsSemiBold",
+        //   "Helvetica",
+        //   "Arial",
+        //   "sans-serif",
+        // ],
+        // poppinsLight: ["PoppinsLight", "Helvetica", "Arial", "sans-serif"],
+        // poppinsMedium: ["PoppinsMedium", "Helvetica", "Arial", "sans-serif"],
+        // poppinsThin: ["PoppinsThin", "Helvetica", "Arial", "sans-serif"],
+        // PloniRegular: ["PloniRegular", "Helvetica", "Arial", "sans-serif"],
+        // PloniMedium: ["PloniMedium", "Helvetica", "Arial", "sans-serif"],
+        // PloniLight: ["PloniLight", "Helvetica", "Arial", "sans-serif"],
+      },
+      fontWeight: {
+        thin: "100",
+        extralight: "200",
+        light: "300",
+        normal: "400",
+        medium: "500",
+        semibold: "600",
+        bold: "700",
+        extrabold: "800",
+        black: "900",
+      },
+      colors: {
+        black: {
+          DEFAULT: "#000",
+          100: "#000319",
+          200: "rgba(17, 25, 40, 0.75)",
+          300: "rgba(255, 255, 255, 0.125)",
+        },
+        blue: {
+          "100": "#E4ECFF",
+        },
+        "coral-red": "#FF6452",
+        "slate-gray": "#6D6D6D",
+        "pale-blue": "#F5F6FF",
+        "white-400": "rgba(255, 255, 255, 0.80)",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        moveHorizontal: {
+          "0%": {
+            transform: "translateX(-50%) translateY(-10%)",
+          },
+          "50%": {
+            transform: "translateX(50%) translateY(10%)",
+          },
+          "100%": {
+            transform: "translateX(-50%) translateY(-10%)",
+          },
+        },
+        moveInCircle: {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "50%": {
+            transform: "rotate(180deg)",
+          },
+          "100%": {
+            transform: "rotate(360deg)",
+          },
+        },
+        moveVertical: {
+          "0%": {
+            transform: "translateY(-50%)",
+          },
+          "50%": {
+            transform: "translateY(50%)",
+          },
+          "100%": {
+            transform: "translateY(-50%)",
+          },
+        },
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        first: "moveVertical 30s ease infinite",
+        second: "moveInCircle 20s reverse infinite",
+        third: "moveInCircle 40s linear infinite",
+        fourth: "moveHorizontal 40s ease infinite",
+        fifth: "moveInCircle 20s ease infinite",
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [
+    // require("tailwindcss-filters"),
+    // require("tailwindcss-animate"),
+    addVariablesForColors,
+    function ({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          "bg-grid": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="75" height="75" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
+            )}")`,
+          }),
+          "bg-grid-small": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
+            )}")`,
+          }),
+          "bg-dot": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`,
+            )}")`,
+          }),
+        },
+        {
+          values: flattenColorPalette(theme("backgroundColor")),
+          type: "color",
+        },
+      );
+    },
+  ],
+} satisfies Config;
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
+export default config;
