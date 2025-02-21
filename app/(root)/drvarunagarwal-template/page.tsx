9d7890f131cf82cf7aa1e3ad238f49c80a17f12e
Rishabh <github@aperta.anonaddy.com>
2025-02-21 08:40:42 +0530

    changed map location to a dermat clinic

--- app/(root)/drvarunagarwal-template/page.tsx
+++ app/(root)/drvarunagarwal-template/page.tsx
@@ -94,12 +94,19 @@ const page = () => {
           </h1>
           <div className="mx-auto mb-2 aspect-square w-[90vw] md:w-[80vw] lg:w-[25vw]">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22582.143987612282!2d-122.1565248820646!3d37.42647615735572!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fbae5f33774b5%3A0x6dd7ef2d054587e3!2sTerun%20Pizza!5e0!3m2!1sen!2sin!4v1735704949104!5m2!1sen!2sin"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d110064.79472718644!2d-121.50756434325555!3d38.57697908980686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x809ae7f918dc015b%3A0xa4dfb8793904b2ea!2sPacific%20Skin%20Institute!5e0!3m2!1sen!2sin!4v1740104697775!5m2!1sen!2sin"
               width="100%"
               height="100%"
               className="rounded-2xl border-0"
               referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
+            {/* <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22582.143987612282!2d-122.1565248820646!3d37.42647615735572!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fbae5f33774b5%3A0x6dd7ef2d054587e3!2sTerun%20Pizza!5e0!3m2!1sen!2sin!4v1735704949104!5m2!1sen!2sin"
+              width="100%"
+              height="100%"
+              className="rounded-2xl border-0"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe> */}
           </div>
           <h1 className="my-5 text-2xl md:text-3xl">Working Hours</h1>
           <div className="mx-auto grid w-[85vw] grid-cols-2 justify-center md:w-[70vw] md:text-xl lg:w-[25vw]">
